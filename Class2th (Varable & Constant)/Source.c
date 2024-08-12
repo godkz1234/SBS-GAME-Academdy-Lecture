7dfaf3988442110e9308e95f20dc26a1ebf8b5cb
godkz1234 <ddddeong@naver.com>
2024-08-12 21:46:16 +0900

    (2024.08.12) 비트

---
+++ Class2th (Varable & Constant)/Source.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+void main()
+{
+#pragma region 변수
+
+	// %C는 한문자를 표현하는 서식 지정자
+	// %d는 정수를 표현하는 서식 지정자
+	// %f는 실수를 표현하는 서식
+	//char alphabet = 'A';
+	//int data = 100;
+	//float decimal = 3.75f;
+
+
+	//alphabet = 'B';
+	//data = 999;
+	//decimal = 5.75f;
+
+	//printf("%c\n", alphabet);
+	//printf("%d\n", data);
+	//printf("%f\n", decimal);
+#pragma endregion
+
+
+#pragma region 상수
+
+	// 프로그램이 실행되는 동안 더 이상 변경할 수  없는
+	// 메모리 공간입니다.
+
+	const float pi = 3.14f;
+
+	// 상수는 메모리 공간을 생성하는 동시에 초기화해야 하며,
+	// 한 번 저장된 값은 더 이상 변경할 수 없습니다.
+
+	//pi = 6.66f;
+
+	// 상수의 경우 메모리 공간을 가지고 있지 않은 상수를 리터럴
+	// 상수라고 하며, 메모리 공간을 가지고 있는 상수를 심볼릭 상수라고 합니다.
+
+	// + - * / %
+	// 1. 변수 (result1) = 변수 + 변수
+	// 2. 변수 (result2) = 변수 - 리터럴 상수
+	// 3. 변수 (result3) = 변수 * 심볼릭 상수
+	// 4. 변수 (result4) = 리터럴 상수 / 심볼릭 상수
+	// 5. 변수 (result5) = 심볼릭 상수 % 심볼릭 상수
+
+	//int A = 30;
+	//int B = 20;
+	//const int C = 6;
+	//const int D = 10;
+
+	//int result1 = A + B;
+	//int result2 = B - 12;
+	//int result3 = B * C;
+	//int result4 = 10 / C;
+	//int result5 = C % D;
+
+
+
+	//printf("result1의 값 : %d\n", result1);
+	//printf("result2의 값 : %d\n", result2);
+	//printf("result3의 값 : %d\n", result3);
+	//printf("result4의 값 : %d\n", result4);
+	//printf("result5의 값 : %d\n", result5);
+#pragma endregion
+
+}
--- Program/Source.c
+++ Program/Source.c
@@ -1,66 +1,118 @@
 #include <stdio.h>
+
 void main()
 {
-#pragma region 변수
+#pragma region 변수의 이름 규칙
+	// 1. 변수의 경우 이미 중복된 변수의 이름을 
+	//	  허용하진 않지만, 변수의 이름을 대소문자로 
+	//    구별하여 사용할 수 있습니다.
 
-	// %C는 한문자를 표현하는 서식 지정자
-	// %d는 정수를 표현하는 서식 지정자
-	// %f는 실수를 표현하는 서식
-	//char alphabet = 'A';
-	//int data = 100;
-	//float decimal = 3.75f;
+	// ex) int data = 10;
+	// ex) int DATA = 20;
 
+	// 2. 변수의 이름으로 예약어를 사용할 수 없습니다.
+	// ex) int int = 10;
 
-	//alphabet = 'B';
-	//data = 999;
-	//decimal = 5.75f;
+	// 3. 변수의 이름은 숫자로 시작할 수 없습니다.
+	// ex) int 1x = 10;
 
-	//printf("%c\n", alphabet);
-	//printf("%d\n", data);
-	//printf("%f\n", decimal);
+	// 4. 변수의 이름으로 공백이 포함될 수 없습니다.
+	// ex) int count down = 10;
+	
+	// 5. 변수의 이름으로 특수 기호는 "_"와"$"만 허용합니다.
+	//int m_attack = 10;
+	//int m$health = 20;
+	//int m!defense = 30;
+
+#pragma endregion
+
+#pragma region 비트
+	// 테이터를 나타내는 최소의 단위이며, 0 또는 1의
+	// 조합으로 논리 계산을 수행하는 단위입니다.
+
+#pragma region (10)진수를 (2)진수로 변환하는 과정
+	// 10진수를 1이 될때까지 계속 2로 나누어 준 다음
+	// 나눈 위치의 나머지 값을 아래에서 위로 순서대로 정렬합니다.
+	// ex) 10진수 15를 2진수를 변환하면?
+	// 결과 : 1111
+	// 과정 : 15를 2로 나누면 몫7에 나머지 1
+	//		  7을 2로 나누면 몫3에 나머지1
+    //	      3을 2로 나누면 몫1에 나머지 1
+	//		  따라서 1111이 나온다.
+#pragma endregion
+
+#pragma region (2)진수를 (10)진수로 변환하는 과정
+	// 1byte에 2진수로 저장된 값을 2의 제곱으로 나타냅니다.
+	
+	// 각각의 비트에 1이 있다면 1과 2의 제곱의 위치를 계산한 다음
+	// 각각의 비트를 모두 더하여 10진수로 나타냅니다.
 #pragma endregion
 
 
-#pragma region 상수
+	// 메모리는 비트 단위로 데이터를 저장할 수 있으며,
+	// 1개의 비트에는 0또는 1의 값만 저장할 수 있습니다.
 
-	// 프로그램이 실행되는 동안 더 이상 변경할 수  없는
-	// 메모리 공간입니다.
-
-	const float pi = 3.14f;
-
-	// 상수는 메모리 공간을 생성하는 동시에 초기화해야 하며,
-	// 한 번 저장된 값은 더 이상 변경할 수 없습니다.
-
-	//pi = 6.66f;
-
-	// 상수의 경우 메모리 공간을 가지고 있지 않은 상수를 리터럴
-	// 상수라고 하며, 메모리 공간을 가지고 있는 상수를 심볼릭 상수라고 합니다.
-
-	// + - * / %
-	// 1. 변수 (result1) = 변수 + 변수
-	// 2. 변수 (result2) = 변수 - 리터럴 상수
-	// 3. 변수 (result3) = 변수 * 심볼릭 상수
-	// 4. 변수 (result4) = 리터럴 상수 / 심볼릭 상수
-	// 5. 변수 (result5) = 심볼릭 상수 % 심볼릭 상수
-
-	int A = 30;
-	int B = 20;
-	const int C = 6;
-	const int D = 10;
-
-	int result1 = A + B;
-	int result2 = B - 12;
-	int result3 = B * C;
-	int result4 = 10 / C;
-	int result5 = C % D;
-
-
-
-	printf("%d\n", result1);
-	printf("%d\n", result2);
-	printf("%d\n", result3);
-	printf("%d\n", result4);
-	printf("%d\n", result5);
 #pragma endregion
+
+#pragma region 비트 연산자
+	// 비트 단위로 논리 연산을 수행하기위해 사용하는
+	// 연산자입니다.
+
+
+#pragma region AND 연산자
+	// 두 개의 피연산자가 모두 1이면 1을 반환하는 연산자입니다.
+	
+
+	//char a = 10;  // 0000 1010
+	//char b = 13;  // 0000 1101
+				  // 0000 1000
+	
+	//printf("a와 b변수를 AND 연산한 결과 : %d\n", a & b);
+
+#pragma endregion
+
+#pragma region OR 연산자
+	// 두 개의 피연산자 중에 하나라도 1이 있다면
+	// 1을 변환하는 연산자입니다.
+
+	//int c = 9;   // 0000 1001
+	//int d = 11;	 // 0000 1011
+                 // 0000 1011
+	 
+
+	//printf("c와 d변수를 OR 연산한 결과 : %d\n", c | d);
+#pragma endregion
+
+#pragma region XOR 연산자
+	// 두 개의 피연산자가 서로 같으면 0을 반환하고,
+	// 서로 다르면 1을 반환하는 연산자입니다.
+
+	//int e = 7; // 0000 0111;
+	//int f = 5; // 0000 0101;
+			     // 0000 0010 ^
+	//printf("e와 f변수를 XOR 연산한 결과 : %d\n", e ^ f);
+
+#pragma endregion
+
+#pragma region NOT 연산자
+	// 하나의 피연산자를 반전시키는 연산자입니다.
+
+	//int g = 20; // 0001 0100
+				// 1110 1011 ~
+
+
+	//  [1]  [1] [1] [0]  [1] [0] [1] [1]
+	// -128  64   32	   8	   2   1
+	// 계산 : -128 + 64 + 32 + 8 + 2 + 1 = -21
+	// 결과 : -21
+	//printf("g변수를 NOT 연산한 결과 : %d\n", ~ g);
+
+#pragma endregion
+
+
+
+#pragma endregion
+
+
 
 }
