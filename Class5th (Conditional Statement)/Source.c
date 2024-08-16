1a0d309e57773783b057e2c927b729e79494dff6
godkz1234 <ddddeong@naver.com>
2024-08-16 21:45:26 +0900

    (2024.08.16) 반복문

---
+++ Class5th (Conditional Statement)/Source.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+void main()
+{
+#pragma region 조건문
+	// 어떤 조건이 주어질 때 해당 조건에 따라
+	// 동작을 수행하도록 실행하는 명령문입니다.
+
+#pragma region 관계 연산자
+	// 두 개의 피연산자의 값을 비교하여 그 결과를 0
+	// 또는 1이라는 값으로 나타내는 연산자입니다.
+
+	//int condition1 = 10 > 0;
+	//int condition2 = 10 < 0;
+	//int condition3 = 10 >= 0;
+	//int condition4 = 10 <= 0;
+	//int condition5 = 10 == 0;
+	//int condition6 = 10 != 0;
+	//
+	//printf("condition1 변수의 값 : %d\n", condition1);
+	//printf("condition2 변수의 값 : %d\n", condition2);
+	//printf("condition3 변수의 값 : %d\n", condition3);
+	//printf("condition4 변수의 값 : %d\n", condition4);
+	//printf("condition5 변수의 값 : %d\n", condition5);
+	//printf("condition6 변수의 값 : %d\n", condition6);
+
+	// 관계 연산자는 조건이 맞을 때 1이라는 값을 반환하며,
+	// 조건이 틀릴 땐 0이라는 값으로 반환합니다
+#pragma endregion
+
+#pragma region if문
+	// 어떤 특정한 조건을 비교하여 조건이 맞다면
+	// 실행되는 명령문입니다.
+
+#pragma endregion
+
+
+
+
+#pragma endregion
+
+}
